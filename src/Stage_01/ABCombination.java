c419a0ba1dd7932f3d00c97ea76c61a41802311d
delta71911 <delta71911@naver.com>
2023-04-17 16:53:19 +0900

    Initial commit

---
+++ src/Stage_01/ABCombination.java
@@ -0,0 +1,15 @@
+package Stage_01;
+import java.util.Scanner;
+public class ABCombination {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int a = stdIn.nextInt();
+        int b = stdIn.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println((a / b);
+        System.out.println((a % b);
+    }
+}
---
+++ src/Stage_01/AplusB.java
@@ -0,0 +1,11 @@
+package Stage_01;
+import java.util.Scanner;
+public class AplusB {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int a = stdIn.nextInt();
+        int b = stdIn.nextInt();
+
+        System.out.println(a + b);
+    }
+}
